68cf5cf9393061f9a9a7c1226dd6e9effc0ce38d
Chenger1 <exs2199@gmail.com>
2021-03-26 19:54:27 +0200

    Add user registration. Also have a little changing on login.html

--- blog/user_app/forms.py
+++ blog/user_app/forms.py
@@ -1,6 +1,39 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username',
+                  'email',
+                  'password1',
+                  'password2',
+                  'first_name',
+                  'last_name']
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+
+        if User.objects.filter(username=username).exists():
+            raise ValidationError({'username': 'Такой пользователь уже существует'})
+        if password1 != password2:
+            raise ValidationError({'password1': 'Пароли не совпадают'})
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
