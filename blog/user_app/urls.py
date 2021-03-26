941294b6cf51f746bfff9f400b842404a4772fa2
Chenger1 <exs2199@gmail.com>
2021-03-26 18:09:23 +0200

    Add UserLogin View and template

--- blog/user_app/urls.py
+++ blog/user_app/urls.py
@@ -1,10 +1,12 @@
-from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+
+from user_app import views
 
 app_name = 'user_app'
 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login_view'),
-    path('logout/', LogoutView.as_view(), name='logout_view')
+    path('login/', views.UserLoginView.as_view(), name='login_view'),
+    path('logout/', views.LogoutView.as_view(), name='logout_view'),
+    path('registration/', views.RegistrationView.as_view(), name='registration_view'),
 ]
--- blog/user_app/views.py
+++ blog/user_app/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.views import View
 
-# Create your views here.
+from user_app.forms import LoginForm
+
+
+class UserLoginView(View):
+    template_name = 'registration/login.html'
+    form = LoginForm
+
+    def get(self, request):
+        form = self.form()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('blog_app:list_view')
+            else:
+                return render(request, self.template_name, {'form': form,
+                                                            'error': 'Неправильный логин или пароль'})
+        else:
+            return render(request, self.template_name, {'form': form,
+                                                        'error': 'Введены некорректные данные'})
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('blog_app:list_view')
+
+
+class RegistrationView(View):
+    pass
