3fd0f12d87412f7f226701ff6a3ae531eeef68d9
Chenger1 <exs2199@gmail.com>
2021-03-26 16:43:57 +0200

    Add user_app to global urls

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog_app.urls', namespace='blog_app'))
+    path('', include('blog_app.urls', namespace='blog_app')),
+    path('user/', include('user_app.urls', namespace='user_app'))
 ]
