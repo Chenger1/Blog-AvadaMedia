7f4709eec5f898919a6224714550ed43a65caff7
Chenger1 <exs2199@gmail.com>
2021-03-26 14:18:59 +0200

    Update admin panel. Add an additional filters for post model

--- blog/blog_app/admin.py
+++ blog/blog_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog_app.models import Post, Category
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_date', 'is_important', 'is_publish', 'category']
+    list_filter = ['author', 'is_important', 'is_publish', 'category']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
