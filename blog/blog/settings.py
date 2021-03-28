a339e56342204b91b8523847f2dfc7ebc9e79bef
Chenger1 <exs2199@gmail.com>
2021-03-28 20:31:43 +0300

    Add postgres db

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 import os
 
-from blog.config import secret_key
+from blog.config import secret_key, password, user
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,9 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'django.contrib.postgres',
+    
     'blog_app',
-    'user_app'
+    'user_app',
 ]
 
 MIDDLEWARE = [
@@ -81,9 +82,15 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': user,
+        'PASSWORD': password
     }
 }
 
